7f0a2c25d0304e28354145a852a486d75c64b697
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-11-21 09:17:19 -0500

    reducer case statements completed

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,4 +1,4 @@
-export {DATA_LOADING,DATA_FETCHED,FETCH_ERROR} from '../actions/index'
+export {IS_LOADING,DATA_FETCHED,FETCH_ERROR} from '../actions/index'
 const initialState = {
     smurfs: [],
     isLoading:false,
@@ -9,6 +9,22 @@ const initialState = {
 export const smurfReducer = (state=initialState,action) => {
     console.log('ol: index.js/reducers: reducer: state, action',state,action)
     switch(action.type){
-        case 
+        case IS_LOADING:
+            return {
+                ...state,
+                isLoading:true
+            }
+        case DATA_FETCHED:
+            return {
+                ...state,
+                smurfs: action.payload
+            }
+        case FETCH_ERROR:
+            return{
+                ...state,
+                error: action.payload
+            }
+        default:
+            return state
     }
 }
