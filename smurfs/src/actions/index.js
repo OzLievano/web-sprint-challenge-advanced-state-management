94df3b6f40f8bde25769b1dd0b39649dd4febd93
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-11-21 09:38:23 -0500

    loading data succesffuly and smurf info rendering

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const IS_LOADING = "IS_LOADING"
 export const DATA_FETCHED = "DATA_FETCHED"
 export const FETCH_ERROR = "FETCH_ERROR"
@@ -5,8 +7,21 @@ export const FETCH_ERROR = "FETCH_ERROR"
 export const loadSmurfData = () => (dispatch) => {
     console.log('ol: index.js actions: dispatch',dispatch)
     const apiURL = 'http://localhost:3333/smurfs'
-    
+
     dispatch({
         type:IS_LOADING
+    })
+
+    axios.get(apiURL)
+    .then((res)=>{
+        console.log(res.data)
+        dispatch({type:DATA_FETCHED,payload:res.data})
+    })
+    .catch((err)=>{
+        console.log(err)
+        dispatch({
+            type:FETCH_ERROR,
+            payload:`error fetching data: ${err.message}`
+        })
     })
 }
